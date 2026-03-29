1fb960cbce0aaf1ac8346b1fa0833946096dc1b2
Igor Hittrich <igor.hittrich@wolt.com>
2026-03-29 22:53:08 +0200

    Add version support

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"os"
 
 	"github.com/mockzilla/connexions/v2/pkg/portable"
 )
 
+var version = "dev"
+
 //go:embed openapi static
 var content embed.FS
 
 func main() {
+	if len(os.Args) == 2 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Println(version)
+		return
+	}
 	os.Exit(portable.RunFS(content, os.Args[1:]))
 }
